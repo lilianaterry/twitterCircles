840aaaddac621fd2e39ad7f29c29be34f1e21dcb
Carter Airhart <airhartcarter@gmail.com>
2017-10-28 22:24:50 -0500

    added some shit to the textanalysis

--- Liliana/textualAnalysis.js
+++ Liliana/textualAnalysis.js
@@ -1,53 +1,85 @@
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 var ds = require('datastructures-js');
+var Twitter = require('twitter');
+var http = require('http');
+var client = new Twitter({
+  consumer_key: '7lqxbqzQHLoA9flVT6o2krPCi',
+  consumer_secret: 'vCBmlGpswlj2eIxGHvvpWmMRtpLhkvprt67M4PrOsywbRiNgIH',
+  access_token_key: '1917957684-EuFf4tPxzhcqvdaqCbIsYUdoEh9yCxUvmhH8iWV',
+  access_token_secret: '3M5kQubsrW1rClzF5xKlqBDl3YzTOuwwOkcLqmo17caVn'
+});
 
-console.log(textualAnalysis("hello"));
+var userdict = [];
+var params = {screen_name: 'dylanistrippin'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      for(var tweet in tweets){
 
-function textualAnalysis (text) {
-  // Setup information
-  var natural_language_understanding = new NaturalLanguageUnderstandingV1({
-    'username': 'a42c5a4a-2534-4d4b-ac4f-c8dd2082dfd8',
-    'password': 'rYOeEclHS8FP',
-    'version_date': '2017-02-27'
-  });
-
-  // Requested response data
-  var parameters = {
-    'text': text,
-    'features': {
-      'categories': {}
-    },
-    'language': 'en'
-  };
-  // weighting formula = (number of times appears * relevance score) / total number of categories
-
-  // Run the analysis
-  natural_language_understanding.analyze(parameters, function(err, response) {
-    if (err)
-      console.log('error:', err);
-    else {
-      //console.log(JSON.stringify(response, null, 2));
-      var jsonObj = JSON.parse(JSON.stringify(response));
-      parseResponse(jsonObj);
-    }
-  });
-
-  // parse the results of the analysis
-  function parseResponse(jsonObj) {
-    var categoryList = [];
-    var categories = jsonObj['categories'];
-
-    // get most specific category
-    for (var index = 0; index < categories.length; index++) {
-      var score = categories[index]['score'];
-      var label = categories[index]['label'];
-      var allLabels = label.split('/');
-      allLabels.forEach(function(entry) {
-        if (entry != '') {
-          categoryList.push(entry);
+        // Setup information
+        var natural_language_understanding = new NaturalLanguageUnderstandingV1({
+          'username': 'a42c5a4a-2534-4d4b-ac4f-c8dd2082dfd8',
+          'password': 'rYOeEclHS8FP',
+          'version_date': '2017-02-27'
+        });
+        // Text to analyze
+        var extractedText = tweets[tweet].text;
+        // Requested response data
+        var parameters = {
+          'text': extractedText,
+          'features': {
+            'keywords': {
+              'limit': 8
+            },
+            'categories': {}
+          },
+          'language': 'en'
+        };
+        // weighting formula = (number of times appears * relevance score) / total number of categories
+        // Run the analysis
+        natural_language_understanding.analyze(parameters, function(err, response) {
+          if (err)
+            console.log('error:', err);
+          else {
+            //console.log(JSON.stringify(response, null, 2));
+            var jsonObj = JSON.parse(JSON.stringify(response));
+            parseResponse(jsonObj);
+          }
+        });
+        // parse the results of the analysis
+        function parseResponse(jsonObj) {
+          var categoryList = [];
+          var keywordList = [];
+          var keywords = jsonObj['keywords'];
+          var categories = jsonObj['categories'];
+          // parse all keywords and store their relevance
+          for (var index = 0; index < keywords.length; index++) {
+            var word = keywords[index]['text'];
+            var relevance = keywords[index]['relevance'];
+            keywordList.push(word);
+          }
+          // get most specific category
+          for (var index = 0; index < categories.length; index++) {
+            var score = categories[index]['score'];
+            var label = categories[index]['label'];
+            var allLabels = label.split('/');
+            allLabels.forEach(function(entry) {
+              if (entry != '') {
+                categoryList.push(entry);
+              }
+            });
+          }
+          for(var item in categoryList){
+            if(userdict[categoryList[item]] != undefined){
+              userdict[categoryList[item]] += 1
+            }
+            else{
+              userdict[categoryList[item]] = 1
+            }
+          }
+          //here we start to go through the items in categoryList
+          
         }
-      });
+
+      }
     }
-    return categoryList;
-  }
-}
+  });
